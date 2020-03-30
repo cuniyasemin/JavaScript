8fc571759d5da5b453b3492d9df5ae737f487ca5
cuniyasemin <cuniyasemin@gmail.com>
2020-03-31 02:21:01 +0300

    degiskenler

---
+++ 1-Basics/scripts.js
@@ -0,0 +1,29 @@
+/*
+Variables are a fundamental concept of every programming language in the world.
+We can say that a variable is like a container in which we can store a value in order to use it over and over again in our code
+*/
+
+var firstName = 'Yasemin';
+
+var lastName = 'Cüni';
+
+var age = 24 ;
+
+/*
+    Primitive Types
+    In javascript, there are 5 different data type:
+    1. Number : Floating, decimals, integers
+    2. String : used for text
+    3. Boolean: Logical data type that can only true or false
+    4. Undefined: Data type of variable that does not have a value yet
+    5: Null: Also means 'non-existent'
+*/
+
+var fullAge = true;
+
+var job;
+
+job = 'Engineer'
+
+
+

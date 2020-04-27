b2749afc6b8859a816b23104cd5feafa77825901
cuniyasemin <yasemin.cuni@segmentify.com>
2020-04-27 11:30:59 +0300

    functions

--- 1-Basics/scripts.js
+++ 1-Basics/scripts.js
@@ -247,3 +247,121 @@ else {
 /*
   They are not exactly false, but they will turn out to be false when evaluated in a if/else condition. 
 */
+
+// var height = 23;
+
+// if (height) {
+//     console.log('Variable is defined.');
+
+// } else {
+//     console.log('Variable has NOT been defined.')
+// }
+
+// //Equality operators 
+// if (height == '23') {
+//     console.log('The == operator does type coercion!');
+// }
+
+
+
+
+
+/*****************************
+ * CODING CHALLENGE 2
+ */
+
+/*
+John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+
+1. Calculate the average score for each team
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
+
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+
+GOOD LUCK 😀
+*/
+
+// var scoreJohn = (89 + 120 + 103) / 3;
+// var scoreMike = (116 + 94 + 123) / 3;
+// var scoreMarry = (97 + 134 + 105) / 3;
+
+// console.log(scoreJohn, scoreMike, scoreMarry);
+
+// var minnig = scoreJohn;
+// var name = "John";
+
+// if (scoreMike > minnig) {
+//     minnig = scoreMike;
+//     name = "Mike"
+// }
+// if (scoreMarry > minnig) {
+//     minnig = scoreMarry;
+//     name = "Marry"
+// }
+
+// console.log(name + '\'s team wins with ' + minnig + ' points.');
+
+
+
+
+
+
+
+/**************************************************************************
+ * Functions
+ */
+
+/**
+ If we have a piece of code that we want to run a lot of times, we can put that into a function instead of writing it over and over again.
+ So functions are like containers that hold same lines of code and we can pass arguments into them and they can then returns us our result.
+ */
+
+// function calculateAge(birthYear) {
+//     return 2020 - birthYear;
+// }
+
+
+// var ageJohn = calculateAge(1995);
+// var ageMike = calculateAge(1990);
+// var ageJarry = calculateAge(1950);
+
+
+// function yearsUntilRetirement(year, firstName) {
+//     var age = calculateAge(year);
+//     var retirement = 65 - age;
+
+//     if (retirement > 0) {
+//         console.log(firstName + ' retires in ' + retirement + ' years.');
+//     } else {
+//         console.log(firstName + ' is already retired.');
+//     }
+// }
+
+
+// yearsUntilRetirement(1995, 'John');
+// yearsUntilRetirement(1990, 'Mike');
+// yearsUntilRetirement(1950, 'Jarry');
+
+
+
+
+
+
+/***********************************************************************************
+ * Function Statements and Experessions
+ */
+
+
+// function declaration 
+// function whatDoYouDo (firstName, job)
+
+
+// function expression
+
+var whatDoYouDo = function(firstName, job) {
+    return 25;
+}
+
+console.log(whatDoYouDo('Yasemin', 'Computer Engineer'));
